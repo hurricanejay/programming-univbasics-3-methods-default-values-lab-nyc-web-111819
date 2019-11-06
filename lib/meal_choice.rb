f6464d0a5956ddbe128cd97a5dc43d93170d68cb
Jay Wen <jaywen89@gmail.com>
2019-11-06 00:36:13 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,18 +1,6 @@
 # Your code here
 def meal_choice(protein="meat", vegetable1, vegetable2)
   
-  vegetable1 = "broccoli"
-  vegetable2 = "macaroni"
-  
-  puts "A plate of #{protein} with #{vegetable1} and #{vegetable2}."
-  
-  return "A plate of #{protein} with #{vegetable1} and #{vegetable2}."
-  
-end
-
-
-def meal_choice(protein, vegetable1, vegetable2)
-
   puts "A plate of #{protein} with #{vegetable1} and #{vegetable2}."
   
   return "A plate of #{protein} with #{vegetable1} and #{vegetable2}."
